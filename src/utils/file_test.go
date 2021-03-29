ce9c57afafc896b6a5ccce2421b7d9931f2b4cae
Arya Stark <541479561@qq.com>
2021-03-29 14:21:22 +0800

    fix: erase sensitvie info

--- src/utils/file_test.go
+++ src/utils/file_test.go
@@ -1,22 +1,26 @@
 package utils
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestWriteDataToExcel(t *testing.T) {
 	// filepath, sheetName string, data []string
+	currentDir, _ := os.Getwd()
 	var tests = []struct {
 		filepath  string
 		sheetName string
 		data      []string
 	}{
-		{"/home/lu/work/personal/go/ly-scheduler/src/test1.xlsx", "Table 1", []string{"今天天气真好呀！#可惜明天要去上班了。,学好 Golang 武装自己!", "今天天气真好呀！,可惜明天要去上班了。,学好 Golang 武装自己!"}},
-		{"/home/lu/work/personal/go/ly-scheduler/src/test2.xlsx", "Table 2", []string{"好好学习#天天向上", "好好学习,天天向上"}},
+		{fmt.Sprintf("%s/test3.xlsx", currentDir), "Table 1", []string{"one#two#three", "one#two#three"}},
+		{fmt.Sprintf("%s/test4.xlsx", currentDir), "Table 2", []string{"first#second", "third#fourth"}},
 	}
 	for _, test := range tests {
 		if err := WriteDataToExcel(test.filepath, test.sheetName, test.data); err != nil {
 			t.Errorf("WriteDataToExcel(%s) failed: %v", test.filepath, err)
 		}
+
 	}
 }
